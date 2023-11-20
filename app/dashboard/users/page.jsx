867e73c226c923b4224369f75d89be86e19d9c87
Huntertk <taufik.khan65@gmail.com>
2023-11-20 19:30:43 +0530

    User products page design is completed

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import Search from '@/app/ui/dashboard/search/Search'
+import styles from '@/app/ui/dashboard/users/users.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import Pagination from '@/app/ui/dashboard/pagination/Pagination'
+
 
 const UsersPage = () => {
   return (
-    <div>UsersPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder={"Search for user...."} />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>CreatedAt</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image 
+                src="/noavatar.png" 
+                alt="image" 
+                width={40} 
+                height={40} 
+                className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>john@doe.com</td>
+            <td>20/11/2023</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>View</button>
+                </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+        <Pagination />
+    </div>
   )
 }
 
